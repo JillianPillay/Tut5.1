705099213e7f23fb9985c66c6c3d1a1cdb9bbcc2
JillianPillay <jillianpillay4@gmail.com>
2016-03-22 22:23:24 +0200

    tut5.1 colour function

---
+++ tut5.1/tut5.1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class card
+{
+public :
+	void colour()
+	{
+		int i;
+		for (i = 1; i <= 10; i++)
+		if (i = i + 2)
+			cout << "red" << endl;
+		else
+			cout << "black" << endl;
+
+	}
+};
